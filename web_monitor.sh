985b802acf69e66a3b7a60e7d6ff12248d665291
mechmank <mechmanktest@gmail.com>
2025-07-28 11:41:34 +0530

    adding crondetails

--- web_monitor.sh
+++ web_monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # check if httpd or nginx is installed in the server and make a entry in a log file
+# Give executable permission - chmod +x web_monitor.sh
+# Schedle a cro job for contineous monitor - crontab -e
+# * * * * * /home/user/web_monitor.sh - Runs the script for every minute
 if 
     systemctl list-units --type=service | grep -q httpd; 
     then 
@@ -18,5 +21,5 @@ if ! systemctl is-active --quiet $service; then
     systemctl start  $service
     echo "$(date) : $service was down" >> /var/log/web_service_monitor.log
 else
-    echo "$(date) : $service is running fine"
+    echo "$(date) : $service is running fine" >> /var/log/web_service_monitor.log
 fi
